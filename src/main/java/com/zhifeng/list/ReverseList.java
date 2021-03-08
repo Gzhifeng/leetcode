ff9825b58e9d3bafd500da44a98f3b21e1f741d0
ganzhifeng <gan987520172@163.com>
2021-03-08 16:12:56 +0800

    反转单向链表

--- src/main/java/com/zhifeng/list/ReverseList.java
+++ src/main/java/com/zhifeng/list/ReverseList.java
@@ -48,12 +48,11 @@ public class ReverseList {
 
         ListNode prev = null;
         ListNode curr = head;
-
         while (curr != null) {
-            ListNode nextTemp = curr.next;
+            ListNode temp = curr.next;
             curr.next = prev;
             prev = curr;
-            curr = nextTemp;
+            curr = temp;
         }
         return prev;
     }
