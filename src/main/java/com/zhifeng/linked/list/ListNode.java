c24481627b1be4a5bc82a66533eb0d3a9c3957f5
ganzhifeng <gan987520172@163.com>
2021-11-18 14:06:38 +0800

    链表练习

---
+++ src/main/java/com/zhifeng/linked/list/ListNode.java
@@ -0,0 +1,13 @@
+package com.zhifeng.linked.list;
+
+/**
+ * @author ganzhifeng
+ * @date 2021/11/17
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
---
+++ src/main/java/com/zhifeng/linked/list/Solution2.java
@@ -0,0 +1,58 @@
+package com.zhifeng.linked.list;
+
+/**
+ * 两数相加
+ *
+ * @author ganzhifeng
+ * @date 2021/11/17
+ */
+public class Solution2 {
+
+    public static void main(String[] args) {
+        ListNode listNode = new ListNode(2);
+        ListNode listNode1 = new ListNode(4);
+        ListNode listNode2 = new ListNode(3);
+
+        listNode.next = listNode1;
+        listNode1.next = listNode2;
+
+
+        ListNode listNode3 = new ListNode(5);
+        ListNode listNode4 = new ListNode(6);
+        ListNode listNode5 = new ListNode(4);
+
+        listNode3.next = listNode4;
+        listNode4.next = listNode5;
+
+        ListNode listNode6 = addTwoNumbers(listNode, listNode3);
+        System.out.println(listNode6);
+    }
+
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null, tail = null;
+        int carray = 0;
+        while (l1 != null || l2 != null) {
+            int n1 = l1 != null ? l1.val : 0;
+            int n2 = l2 != null ? l2.val : 0;
+            int sum = n1 + n2 + carray;
+            if (head == null) {
+                head = tail = new ListNode(sum % 10);
+            } else {
+                tail.next = new ListNode(sum % 10);
+                tail = tail.next;
+            }
+            carray = sum / 10;
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+            if (carray > 0) {
+                tail.next = new ListNode(carray);
+            }
+        }
+        return head;
+    }
+
+}
