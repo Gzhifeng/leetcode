5acdcd4a27e26fd43e32b1895950f918d8ee04a6
ganzhifeng <gan987520172@163.com>
2021-11-24 20:18:51 +0800

    #

---
+++ src/main/java/com/zhifeng/linked/list/Solution21.java
@@ -0,0 +1,94 @@
+package com.zhifeng.linked.list;
+
+/**
+ * 合并两个有序链表
+ * 
+ * 作者：LeetCode-Solution
+ * 链接：https://leetcode-cn.com/problems/longest-harmonious-subsequence/solution/zui-chang-he-xie-zi-xu-lie-by-leetcode-s-8cyr/
+ * 来源：力扣（LeetCode）
+ * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+ * 
+ * @author ganzhifeng
+ * @date 2021/11/23
+ */
+public class Solution21 {
+
+    public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode megeTwoList(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode megerTwoList2(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = megerTwoList2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = megerTwoList2(l1, l2.next);
+            return l2;
+        }
+    }
+
+
+    public ListNode megreList2(ListNode l1, ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else  if (l1.val < l2.val) {
+            l1.next = megreList2(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = megreList2(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public ListNode mergeTwoLists2(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+        return prehead.next;
+    }
+}
